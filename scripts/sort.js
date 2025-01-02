871e3c104ca73f48539f883d6f7814d21e548afa
Yutiy Kulikov <kulikov-y@yandex.ru>
2025-01-02 08:40:55 +0300

    добавлена сортировка таблицы вещей

---
+++ scripts/sort.js
@@ -0,0 +1,42 @@
+'use strict'
+console.log('Sort.js работает')
+
+
+
+const tableSort = (idTable) => {
+
+  const table = document.querySelector(idTable);
+  console.log(table.querySelectorAll('tr'))
+  let sortedRows = Array.from(table.rows)
+    .slice(1)
+  sortedRows.forEach((str) => {
+
+    str.insertCell(0);
+    str.cells[0].innerText = `${str.cells[1].innerText}${str.cells[2].innerText}`;
+    str.cells[0].style.display = 'none';
+  })
+
+  console.log(sortedRows);
+
+  // sortedRows.sort((rowA, rowB,) => rowA.cells[0].innerText > rowB.cells[0].innerText);
+  sortedRows.sort((rowA, rowB,) => rowA.cells[0].innerText < rowB.cells[0].innerText ? 1 : -1);
+
+
+  table.tBodies[0].append(...sortedRows);
+
+}
+
+
+
+// document.querySelector('.close').addEventListener('click', () => {
+//   tableSort(idTable);
+
+// })
+
+
+try {
+  tableSort('#thingsAndPlaсes');
+} catch (error) {
+  console.log('"Таблица вещей" недоступна для сортировки');
+}
+
