e06fcdd729d7fcb74cea9e4b845ead4b7f7a0b08
Yutiy Kulikov <kulikov-y@yandex.ru>
2025-01-02 12:46:32 +0300

    добавил класс wrwpper + эксперименты со стилями

--- scripts/clearLs.js
+++ scripts/clearLs.js
@@ -2,12 +2,25 @@
 console.log('clearls в работе');
 const getClearLs = (idTable) => {
     localStorage.removeItem(idTable)
+    location.reload()
 }
 
 // Удаление из LS таблицы вещей
 try {
     document.querySelector('#clearLsThihks').addEventListener('click', () => {
         getClearLs('#thingsAndPlaсes')
+
+    })
+
+} catch (error) {
+    console.log('удаление Вещей из LS не работает');
+
+}
+
+// Удаление из LS таблицы МЕСТ
+try {
+    document.querySelector('#clearLsPlace').addEventListener('click', () => {
+        getClearLs('#plase')
 
     })
 
