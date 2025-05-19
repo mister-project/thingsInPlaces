5daed464be762c991c2b66dd7cce433ca3c253a3
Yutiy Kulikov <kulikov-y@yandex.ru>
2025-05-19 13:52:53 +0300

    ошибка в работе фильтра

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,39 +1,36 @@
-'use strict'
-console.log('filtr.js в работе')
+"use strict";
+console.log("filtr.js в работе");
 
 const getFilter = (idInput, idTable) => {
-    const filter = document.querySelector(idInput);
-    let strTable = document.querySelector(idTable).querySelector('tbody').querySelectorAll('tr')
+  const filter = document.querySelector(idInput);
+  let strTable = document
+    .querySelector(idTable)
+    .querySelector("tbody")
+    .querySelectorAll("tr");
 
-    // let strTableNo = document.querySelectorAll('.trNoFilter')
-    // // console.log(document.querySelector('span'))
+  console.log(strTable);
+  // let strTableNo = document.querySelectorAll('.trNoFilter')
+  // // console.log(document.querySelector('span'))
 
-    filter.addEventListener('input', (e) => {
-
-        //     strTableNo.forEach((el) => {
-        //         el.style.display = "none"
-        //         console.log(el);
-        //     })
-        strTable.forEach(element => {
-
-            if (element.textContent.toUpperCase().includes((e.target.value).toUpperCase())) {
-                element.style.display = "";
-
-
-
-            } else {
-                element.style.display = "none"
-            }
-        });
-
-    })
-
+  filter.addEventListener("input", (e) => {
+    //     strTableNo.forEach((el) => {
+    //         el.style.display = "none"
+    //         console.log(el);
+    //     })
+    strTable.forEach((element) => {
+      if (
+        element.textContent.toUpperCase().includes(e.target.value.toUpperCase())
+      ) {
+        element.style.display = "";
+      } else {
+        element.style.display = "none";
+      }
+    });
+  });
 };
 // Фильтр для строк таблицы вещей
 try {
-    getFilter('#filter', "#thingsAndPlaсes")
+  getFilter("#filter", "#thingsAndPlaсes");
 } catch (error) {
-    console.log('Не работает фильтр для таблицы вещей');
-
+  console.log("Не работает фильтр для таблицы вещей");
 }
-
